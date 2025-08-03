d73442e7a4a3a5ed4fd2b7857d9f26ca7b5f9135
aokigahara <alex.lukianov.12@gmail.com>
2025-08-03 20:21:00 +0300

    LF-5 feat: временный migrator

--- src/Services/Researcher/Infrastrcuture/Researcher.Api/Program.cs
+++ src/Services/Researcher/Infrastrcuture/Researcher.Api/Program.cs
@@ -1,6 +1,8 @@
+using Microsoft.EntityFrameworkCore;
 using Researcher.Api.Common.Extensions;
 using Researcher.Api.Common.Extensions.EndpointsExtensions;
 using Researcher.Api.Common.Middlewares;
+using Researcher.Infrastructure.DAL;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +12,13 @@ builder.Services
     .AddWebServices();
 
 var app = builder.Build();
+
+// Временный мигратор базы данных
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<ResearcherDbContext>();
+    await dbContext.Database.MigrateAsync();
+}
 
 app.UseWebServices();
 app.MapApiEndpoints();
